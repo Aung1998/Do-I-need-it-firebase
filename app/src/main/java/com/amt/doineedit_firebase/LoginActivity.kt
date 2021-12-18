cb312a808dbc396ae47ad3f66ad15fa12a3d23de
Aung Myat Thu <aungmyatthu1998@gmail.com>
2021-12-18 17:49:18 +0630

    Login finished

--- app/src/main/java/com/amt/doineedit_firebase/LoginActivity.kt
+++ app/src/main/java/com/amt/doineedit_firebase/LoginActivity.kt
@@ -1,7 +1,12 @@
 package com.amt.doineedit_firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,12 +18,34 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener{
+            val email = findViewById<TextInputLayout>(R.id.textLayout_email)
+                .editText?.text.toString()
+
+            val password = findViewById<TextInputLayout>(R.id.textLayout_password)
+                .editText?.text.toString()
+
+            if(email.isBlank() || password.isBlank()){
+                Toast.makeText(this, "Please Enter Email or Password", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                accountLogin(email, password)
+            }
+        }
+    }
+
+    private fun accountLogin(email:String, password:String) {
         auth = Firebase.auth
-
-        val email = findViewById<TextInputLayout>(R.id.textLayout_email)
-            .editText?.text.toString()
-
-        val password = findViewById<TextInputLayout>(R.id.textLayout_password)
-            .editText?.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                //val intent = Intent(this, )
+            }
+        }
+            .addOnFailureListener {
+                Toast.makeText(applicationContext, "Wrong email or password", Toast.LENGTH_LONG).show()
+            }
     }
 }
